32eafa3f0dfa94401a4fecf3d696183c11b7942e
rwise <robertwise64@gmail.com>
2020-12-31 02:56:37 -0600

    Pseudo-content and content placement

--- src/about.js
+++ src/about.js
@@ -4,37 +4,12 @@ export default function About() {
     return (
         <div className={"generic-article"}>
             <h2>About</h2>
-            <p>This will eventually contain info about me... hopefully.</p>
-            <p>This will eventually contain info about me... hopefully.</p>
-            <p>This will eventually contain info about me... hopefully.</p>
-            <p>This will eventually contain info about me... hopefully.</p>
-            <p>This will eventually contain info about me... hopefully.</p>
-            <p>This will eventually contain info about me... hopefully.</p>
-            <p>This will eventually contain info about me... hopefully.</p>
-            <p>This will eventually contain info about me... hopefully.</p>
-            <p>This will eventually contain info about me... hopefully.</p>
-            <p>This will eventually contain info about me... hopefully.</p>
-            <p>This will eventually contain info about me... hopefully.</p>
-            <p>This will eventually contain info about me... hopefully.</p>
-            <p>This will eventually contain info about me... hopefully.</p>
-            <p>This will eventually contain info about me... hopefully.</p>
-            <p>This will eventually contain info about me... hopefully.</p>
-            <p>This will eventually contain info about me... hopefully.</p>
-            <p>This will eventually contain info about me... hopefully.</p>
-            <p>This will eventually contain info about me... hopefully.</p>
-            <p>This will eventually contain info about me... hopefully.</p>
-            <p>This will eventually contain info about me... hopefully.</p>
-            <p>This will eventually contain info about me... hopefully.</p>
-            <p>This will eventually contain info about me... hopefully.</p>
-            <p>This will eventually contain info about me... hopefully.</p>
-            <p>This will eventually contain info about me... hopefully.</p>
-            <p>This will eventually contain info about me... hopefully.</p>
-            <p>This will eventually contain info about me... hopefully.</p>
-            <p>This will eventually contain info about me... hopefully.</p>
-            <p>This will eventually contain info about me... hopefully.</p>
-            <p>This will eventually contain info about me... hopefully.</p>
-            <p>This will eventually contain info about me... hopefully.</p>
-            <p>This will eventually contain info about me... hopefully.</p>
+            <p>Howdy! My name's Rob, and I'm just getting started in my journey as a developer. This site is a bit devoid of content and basic functionality, but we'll get there eventually. For now, enjoy some classic placeholder text as I try to tweak the look and feel of my site.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tellus elementum sagittis vitae et leo. Egestas erat imperdiet sed euismod nisi porta lorem mollis. At consectetur lorem donec massa sapien faucibus et molestie ac. Nullam ac tortor vitae purus faucibus ornare suspendisse. Porttitor eget dolor morbi non arcu risus. Pharetra massa massa ultricies mi quis hendrerit dolor. Pellentesque nec nam aliquam sem. Felis eget nunc lobortis mattis aliquam faucibus purus in massa. Dignissim convallis aenean et tortor at risus viverra adipiscing. Nullam non nisi est sit amet facilisis.</p>
+            <p>Lacinia quis vel eros donec ac. Massa ultricies mi quis hendrerit dolor magna eget. Ut eu sem integer vitae justo eget. Eget felis eget nunc lobortis mattis. Erat imperdiet sed euismod nisi porta lorem. Viverra mauris in aliquam sem fringilla ut morbi tincidunt. Quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor. Diam sollicitudin tempor id eu nisl nunc mi ipsum. Tristique senectus et netus et malesuada fames. Imperdiet sed euismod nisi porta lorem. Malesuada proin libero nunc consequat interdum. Feugiat in ante metus dictum. Sagittis vitae et leo duis ut. Id leo in vitae turpis. Vulputate eu scelerisque felis imperdiet proin fermentum leo vel orci.</p>
+            <p>Proin sed libero enim sed faucibus turpis. Magna fringilla urna porttitor rhoncus dolor. Vel facilisis volutpat est velit. Ornare arcu dui vivamus arcu felis bibendum ut tristique et. Risus in hendrerit gravida rutrum quisque non tellus orci. Vitae suscipit tellus mauris a diam. A diam maecenas sed enim ut sem viverra aliquet eget. Non odio euismod lacinia at quis risus sed vulputate. Venenatis lectus magna fringilla urna porttitor rhoncus. Sed adipiscing diam donec adipiscing.</p>
+            <p>Augue ut lectus arcu bibendum at varius vel pharetra vel. Dictum sit amet justo donec. Curabitur vitae nunc sed velit dignissim sodales ut eu sem. In arcu cursus euismod quis viverra. Sed velit dignissim sodales ut eu sem integer vitae justo. Tincidunt id aliquet risus feugiat. Rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque. Nibh sit amet commodo nulla facilisi. Vulputate mi sit amet mauris commodo. Nibh cras pulvinar mattis nunc sed blandit libero volutpat sed. Euismod quis viverra nibh cras.</p>
+            <p>Duis ut diam quam nulla porttitor. Viverra ipsum nunc aliquet bibendum enim facilisis gravida neque. Facilisi nullam vehicula ipsum a arcu cursus. Sapien et ligula ullamcorper malesuada. Proin nibh nisl condimentum id venenatis. Neque sodales ut etiam sit amet nisl. Porttitor leo a diam sollicitudin tempor id eu nisl. Arcu dui vivamus arcu felis bibendum ut tristique et. Aenean euismod elementum nisi quis eleifend quam adipiscing vitae. Sagittis eu volutpat odio facilisis. Dictum fusce ut placerat orci nulla pellentesque dignissim enim sit. Ultricies tristique nulla aliquet enim. Dictum varius duis at consectetur lorem. Placerat duis ultricies lacus sed turpis tincidunt id aliquet risus. Mattis aliquam faucibus purus in. Sed vulputate mi sit amet mauris commodo quis imperdiet massa. Blandit turpis cursus in hac habitasse platea. Bibendum at varius vel pharetra vel turpis nunc eget. Semper auctor neque vitae tempus quam. Nisi vitae suscipit tellus mauris a diam maecenas sed.</p>
         </div>
     );
 }
