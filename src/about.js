2c8aebc0f9f18c71667d356d6d9373c108791d18
rwise <robertwise64@gmail.com>
2021-01-04 01:35:59 -0600

    About General Content

--- src/about.js
+++ src/about.js
@@ -4,12 +4,43 @@ export default function About() {
     return (
         <div className={"generic-article"}>
             <h2>About</h2>
-            <p>Howdy! My name's Rob, and I'm just getting started in my journey as a developer. This site is a bit devoid of content and basic functionality, but we'll get there eventually. For now, enjoy some classic placeholder text as I try to tweak the look and feel of my site.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tellus elementum sagittis vitae et leo. Egestas erat imperdiet sed euismod nisi porta lorem mollis. At consectetur lorem donec massa sapien faucibus et molestie ac. Nullam ac tortor vitae purus faucibus ornare suspendisse. Porttitor eget dolor morbi non arcu risus. Pharetra massa massa ultricies mi quis hendrerit dolor. Pellentesque nec nam aliquam sem. Felis eget nunc lobortis mattis aliquam faucibus purus in massa. Dignissim convallis aenean et tortor at risus viverra adipiscing. Nullam non nisi est sit amet facilisis.</p>
-            <p>Lacinia quis vel eros donec ac. Massa ultricies mi quis hendrerit dolor magna eget. Ut eu sem integer vitae justo eget. Eget felis eget nunc lobortis mattis. Erat imperdiet sed euismod nisi porta lorem. Viverra mauris in aliquam sem fringilla ut morbi tincidunt. Quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor. Diam sollicitudin tempor id eu nisl nunc mi ipsum. Tristique senectus et netus et malesuada fames. Imperdiet sed euismod nisi porta lorem. Malesuada proin libero nunc consequat interdum. Feugiat in ante metus dictum. Sagittis vitae et leo duis ut. Id leo in vitae turpis. Vulputate eu scelerisque felis imperdiet proin fermentum leo vel orci.</p>
-            <p>Proin sed libero enim sed faucibus turpis. Magna fringilla urna porttitor rhoncus dolor. Vel facilisis volutpat est velit. Ornare arcu dui vivamus arcu felis bibendum ut tristique et. Risus in hendrerit gravida rutrum quisque non tellus orci. Vitae suscipit tellus mauris a diam. A diam maecenas sed enim ut sem viverra aliquet eget. Non odio euismod lacinia at quis risus sed vulputate. Venenatis lectus magna fringilla urna porttitor rhoncus. Sed adipiscing diam donec adipiscing.</p>
-            <p>Augue ut lectus arcu bibendum at varius vel pharetra vel. Dictum sit amet justo donec. Curabitur vitae nunc sed velit dignissim sodales ut eu sem. In arcu cursus euismod quis viverra. Sed velit dignissim sodales ut eu sem integer vitae justo. Tincidunt id aliquet risus feugiat. Rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque. Nibh sit amet commodo nulla facilisi. Vulputate mi sit amet mauris commodo. Nibh cras pulvinar mattis nunc sed blandit libero volutpat sed. Euismod quis viverra nibh cras.</p>
-            <p>Duis ut diam quam nulla porttitor. Viverra ipsum nunc aliquet bibendum enim facilisis gravida neque. Facilisi nullam vehicula ipsum a arcu cursus. Sapien et ligula ullamcorper malesuada. Proin nibh nisl condimentum id venenatis. Neque sodales ut etiam sit amet nisl. Porttitor leo a diam sollicitudin tempor id eu nisl. Arcu dui vivamus arcu felis bibendum ut tristique et. Aenean euismod elementum nisi quis eleifend quam adipiscing vitae. Sagittis eu volutpat odio facilisis. Dictum fusce ut placerat orci nulla pellentesque dignissim enim sit. Ultricies tristique nulla aliquet enim. Dictum varius duis at consectetur lorem. Placerat duis ultricies lacus sed turpis tincidunt id aliquet risus. Mattis aliquam faucibus purus in. Sed vulputate mi sit amet mauris commodo quis imperdiet massa. Blandit turpis cursus in hac habitasse platea. Bibendum at varius vel pharetra vel turpis nunc eget. Semper auctor neque vitae tempus quam. Nisi vitae suscipit tellus mauris a diam maecenas sed.</p>
+            <p>
+            Howdy! My name's Rob, and I'm just getting started on my journey as a developer. This site is a bit devoid of content and basic functionality, 
+            but we'll get there eventually. I aim for this website to be a showcase of anything and everything I've been working on for the past few years,
+            in an effort to be a creator rather than a consumer. It might end up being a bit... unprofessional, but I hope to use this site as a bit of a
+            playground, as I doubt I can expand my coding capabilities with a couple of generic pages.
+            </p>
+            <p>
+            I started my studies at Iowa State University in 2017, pursuing a B.S. in Software Engineering and a B.A. in music. While this might be a more 
+            unconventional approach to higher education, I decided early on that music was a bit too important to me to let go.
+            </p>
+            <p>
+            Much of my programming experience extends from my time at Iowa State University. I have been working with Java since high school in mostly academic
+            settings; due to this, I have plenty of experience implementing data structures and algorithms in Java. It's also my choice language for
+            technical interviews or general competitive programming problems, which I've naturally had to spend a lot of time practicing to get remotely competent.
+            </p>
+            <p>
+            I started my work as a web developer at ISU's Office of Student Financial Aid. As an OSFA developer, I help create and maintain websites and
+            applications for various offices. Given the nature of our content, most of my work is in front end (HTML, CSS, JavaScript/TypeScript, React, Angular),
+            but I get to touch back end work (PHP, Laravel, MariaDB) <i>just</i> enough to call myself a full stack developer. I also get to mess around with GitLab's 
+            CI/CD pipelines on occasion, as well as a touch of Docker (thanks to the sudden shift to remote work). Our team is solely comprised of a few ISU 
+            Students, which means we have a lot of freedom to implement as we see fit. We often have to work directly with our customers to produce coherent 
+            applications, so I'm no stranger to ambiguity and autonomy.
+            </p>
+            <p>
+            As for my music studies, I'm primarily a voice student. I've participated in the Iowa State Singers since starting at ISU, and I've joined in a few
+            musical and opera productions as well. I also enjoy playing electric bass and more recently piano in what remains of my free time. While many might not 
+            see the inherent value in pursuing this second facet of mine, I can say without a doubt that these studies have been instrumental (pun intended) to my
+            growth as a developer and a person as a whole. My explorations in music let me work with a multitude of different personalities, giving me fantastic
+            exposure to the world outside of engineering. In addition, music stretches my communication abilities, as we often discuss abstract concepts, ambiguous 
+            techniques, and emotional expression. Lastly, music has given me the all-important drive to always learn, as you can only hope to improve with an open-mind
+            and the willingness to put in the practice.
+            </p>
+            <p>
+            Well, that's me in around 600 words or less. Of course, there's always more to know about a person, but I find that's best found through direct communication
+            (or perhaps, even an interview). If you made it this far, thanks for your time! Hopefully there's a bit more to explore on this website by the time you're 
+            reading this.
+            </p>
         </div>
     );
 }
