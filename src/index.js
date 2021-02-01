2ba3a5d641322616bf7788a157278a15c2ac30c3
rwise <robertwise64@gmail.com>
2021-01-31 22:41:34 -0600

    Starting with a Bang Blog Post

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import {
     faCalendarCheck,
     faMicrophoneAlt,
     faHouseDamage,
+    faGlassCheers,
 } from "@fortawesome/free-solid-svg-icons";
 
 import App from "./app";
@@ -33,6 +34,7 @@ library.add(
     faCalendarCheck,
     faMicrophoneAlt,
     faHouseDamage,
+    faGlassCheers,
 );
 
 /*
