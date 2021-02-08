cfda56956644ab56b1590aa8ae075926eb83bf4c
rwise <robertwise64@gmail.com>
2021-02-08 00:33:51 -0600

    Added temporary profile image

--- src/about.js
+++ src/about.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ArticleContent from "./templates/articleContent";
 
+import profilePhoto from "./content/images/god_ussop.jpg"
 import "./templates/genericPost.css"
 import "./about.css";
 
@@ -8,7 +9,7 @@ export default function About() {
     return (
         <article className={"about"}>
             <div className={"about-header"}>
-                test test
+                <img className={"about-photo"} src={profilePhoto} />
             </div>
             <div className={"about-content"}>
                 <div className={"generic-post-header"}>
