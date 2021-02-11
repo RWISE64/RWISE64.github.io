de5ad76b31f45a33d2f007ce27de05f3c3a01677
rwise <robertwise64@gmail.com>
2021-02-10 22:55:18 -0600

    Added basic header info

--- src/about.js
+++ src/about.js
@@ -5,11 +5,17 @@ import profilePhoto from "./content/images/god_ussop.jpg"
 import "./templates/genericPost.css"
 import "./about.css";
 
+
 export default function About() {
     return (
         <article className={"about"}>
             <div className={"about-header"}>
                 <img className={"about-photo"} src={profilePhoto} />
+                <div className={"about-header-info"}>
+                    <div className={"about-header-title"}>Robert Wise</div>
+                    <div className={"about-header-subtitle"}>Fledgling developer, classical singer, nap enthusiast</div>
+                    {/* TODO Add social media / github */}
+                </div>
             </div>
             <div className={"about-content"}>
                 <div className={"generic-post-header"}>
